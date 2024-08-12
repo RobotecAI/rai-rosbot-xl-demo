dad43effd3211ef5f0d05ab415f9777e191fb735
Bartłomiej Boczek <bartlomiej.boczek@robotec.ai>
2024-08-12 16:42:13 +0200

    update run-* scripts
    Signed-off-by: Bartłomiej Boczek <bartlomiej.boczek@robotec.ai>

--- run-game.sh
+++ run-game.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 . /opt/ros/jazzy/setup.bash
-export ROS_DOMAIN_ID=61
-export ROS_LOCALHOST_ONLY=1
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=$HOME/.fastrtps_conf.xml 
 xvfb-run ./Project/build/linux/bin/profile/RobotVacuumSample.GameLauncher -bg_ConnectToAssetProcessor=0 --rhi=null
--- run-nav.sh
+++ run-nav.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash 
 . /opt/ros/jazzy/setup.bash
 
-export ROS_LOCALHOST_ONLY=1
-export ROS_DOMAIN_ID=61
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=$HOME/.fastrtps_conf.xml 
 # export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
