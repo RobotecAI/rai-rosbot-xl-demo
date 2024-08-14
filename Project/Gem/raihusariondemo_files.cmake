e798627792e899a8c30f398fc4ea132be5229fae
Jan Hanca <jan.hanca@robotec.ai>
2024-08-14 16:25:50 +0200

    Add O3DE project (#4)
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
    Co-authored-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>

--- Project/Gem/raihusariondemo_files.cmake
+++
@@ -1,7 +0,0 @@
-
-set(FILES
-    Include/RAIHusarionDemo/RAIHusarionDemoBus.h
-    Include/RAIHusarionDemo/RAIHusarionDemoTypeIds.h
-    Source/RAIHusarionDemoSystemComponent.cpp
-    Source/RAIHusarionDemoSystemComponent.h
-)
---
+++ setup_submodules.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Pull RobotVacuumSample repository and apply patches
+git submodule init
+git submodule update
+
+cd ./Project
+git lfs install
+git lfs pull
+
+git apply ../patches/updates.patch
+if [ $? -ne 0 ]
+then
+	echo "Failed to apply patch on a submodule."
+	exit 1
+fi
+
+# Remove unused (ambiguous) files from the submodule
+rm -rf ./Assets/Importer/
+rm -rf ./Assets/RoboVac/
+rm -rf ./Docker
+rm ./README.md
+rm ./build.sh
+
+echo "Submodules updated."
+exit 0
