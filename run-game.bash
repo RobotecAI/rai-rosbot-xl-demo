67a44bee6337dee467a4b81e699277051e914fc7
Jan Hanca <jan.hanca@robotec.ai>
2024-08-22 13:43:59 +0200

    Code review fixes
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- run-game.bash
+++ run-game.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /opt/ros/${ROS_DISTRO}/setup.bash
-./Project/build/linux/bin/profile/RAIROSBotXLDemo.GameLauncher -bg_ConnectToAssetProcessor=0 --rhi=null
+./Project/build/linux/bin/profile/RAIROSBotXLDemo.GameLauncher -bg_ConnectToAssetProcessor=0
