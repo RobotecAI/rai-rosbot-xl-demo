c4c7c5c015b41c7feaf47bcac8ed8665d5778e4f
Jan Hanca <jan.hanca@robotec.ai>
2024-10-07 14:07:18 +0200

    Add license info
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- patches/LEDStrip.cpp
+++ patches/LEDStrip.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Robotec.ai.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 
 #include "LEDStrip.h"
 
