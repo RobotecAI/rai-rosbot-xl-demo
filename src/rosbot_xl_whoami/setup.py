c71752c088425b73b26191ef2f55d2bdecf218f4
Maciej Majek <46171033+maciejmajek@users.noreply.github.com>
2024-10-18 17:20:29 +0200

    chore: update rosbot-xl-whoami version (#24)

--- src/rosbot_xl_whoami/setup.py
+++ src/rosbot_xl_whoami/setup.py
@@ -7,7 +7,7 @@ package_name = "rosbot_xl_whoami"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="1.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         (os.path.join("share", package_name, os.path.dirname(p)), [p])
