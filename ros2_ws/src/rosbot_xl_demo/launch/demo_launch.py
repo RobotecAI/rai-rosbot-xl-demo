fe6eb876df0a3374f397a8d7d42acdecccd897ae
Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
2025-04-25 16:34:41 +0200

    Add nav2 package for multiple robots
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>

---
+++ ros2_ws/src/rosbot_xl_demo/launch/demo_launch.py
@@ -0,0 +1,193 @@
+# Copyright (c) 2023 LG Electronics.
+# Copyright (c) 2024 Open Navigation LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+from pathlib import Path
+import tempfile
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    GroupAction,
+    IncludeLaunchDescription,
+    LogInfo,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.conditions import IfCondition
+from launch.event_handlers import OnShutdown
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from nav2_common.launch import ParseMultiRobotPose
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """
+    Bring up the multi-robots with given launch arguments.
+
+    Launch arguments consist of robot name(which is namespace) and pose for initialization.
+    Keep general yaml format for pose information.
+    ex) robots:='robot1={x: 1.0, y: 1.0, yaw: 1.5707}; robot2={x: 1.0, y: 1.0, yaw: 1.5707}'
+    ex) robots:='robot3={x: 1.0, y: 1.0, z: 1.0, roll: 0.0, pitch: 1.5707, yaw: 1.5707};
+                 robot4={x: 1.0, y: 1.0, z: 1.0, roll: 0.0, pitch: 1.5707, yaw: 1.5707}'
+    """
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('rosbot_xl_demo')
+    launch_dir = os.path.join(bringup_dir, 'launch')
+
+    # On this example all robots are launched with the same settings
+    map_yaml_file = LaunchConfiguration('map')
+    params_file = LaunchConfiguration('params_file')
+    autostart = LaunchConfiguration('autostart')
+    rviz_config_file = LaunchConfiguration('rviz_config')
+    use_rviz = LaunchConfiguration('use_rviz')
+    log_settings = LaunchConfiguration('log_settings', default='true')
+    scene_config = LaunchConfiguration('scene_config')
+
+    # Declare the launch arguments
+    declare_scene_config_file_cmd = DeclareLaunchArgument(
+        'scene_config',
+        default_value=os.path.join(bringup_dir, 'config', 'one_rosbot.yaml'),
+        description='Full path to the scene config file to use',
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value='',
+        description='Full path to map file to load',
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(
+            bringup_dir, 'params', 'nav2_params.yaml'
+        ),
+        description='Full path to the ROS2 parameters file to use for all launched nodes',
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart',
+        default_value='true',
+        description='Automatically startup the stacks',
+    )
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=os.path.join(bringup_dir, 'rviz', 'nav2_namespaced_view.rviz'),
+        description='Full path to the RVIZ config file to use.',
+    )
+
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz', default_value='True', description='Whether to start RVIZ'
+    )
+
+    robots_list = ParseMultiRobotPose('robots').value()
+
+    # Define commands for launching the navigation instances
+    bringup_cmd_group = []
+    for robot_name in robots_list:
+        init_pose = robots_list[robot_name]
+        group = GroupAction(
+            [
+                LogInfo(
+                    msg=[
+                        'Launching namespace=',
+                        robot_name,
+                        ' init_pose=',
+                        str(init_pose),
+                    ]
+                ),
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(launch_dir, 'rviz_launch.py')
+                    ),
+                    condition=IfCondition(use_rviz),
+                    launch_arguments={
+                        'namespace': TextSubstitution(text=robot_name),
+                        'use_namespace': 'True',
+                        'rviz_config': rviz_config_file,
+                    }.items(),
+                ),
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(bringup_dir, 'launch', 'bringup_launch.py')
+                    ),
+                    launch_arguments={
+                        'namespace': robot_name,
+                        'use_namespace': 'True',
+                        'use_composition': 'True',
+                        'slam': 'True',
+                        'use_localization': 'True',
+                        'map': map_yaml_file,
+                        'use_sim_time': 'True',
+                        'params_file': params_file,
+                        'autostart': autostart,
+                        'use_rviz': 'False',
+                        'use_simulator': 'False',
+                        'headless': 'False',
+                    }.items(),
+                ),
+                Node(
+                    package='rosbot_xl_demo',
+                    executable='robot_spawner',
+                    output='screen',
+                    parameters= [
+                        {"robot_name": "rosbot_xl"},
+                        {"robot_namespace": robot_name},
+                        {"robot_initial_position": [init_pose['x'], init_pose['y'], init_pose['z']]}
+                    ]
+                ),
+            ]
+        )
+
+        bringup_cmd_group.append(group)
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+
+    ld.add_action(LogInfo(msg=['number_of_robots=', str(len(robots_list))]))
+
+    ld.add_action(
+        LogInfo(condition=IfCondition(log_settings), msg=['map yaml: ', map_yaml_file])
+    )
+    ld.add_action(
+        LogInfo(condition=IfCondition(log_settings), msg=['params yaml: ', params_file])
+    )
+    ld.add_action(
+        LogInfo(
+            condition=IfCondition(log_settings),
+            msg=['rviz config file: ', rviz_config_file],
+        )
+    )
+    ld.add_action(
+        LogInfo(condition=IfCondition(log_settings), msg=['autostart: ', autostart])
+    )
+
+    for cmd in bringup_cmd_group:
+        ld.add_action(cmd)
+
+    return ld
---
+++ ros2_ws/src/rosbot_xl_demo/setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'rosbot_xl_demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'params'), glob(os.path.join('params', '*.[yaml]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kdabrowski',
+    maintainer_email='kacper.dabrowski@robotec.ai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
